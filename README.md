0dfbc799e656b171bdd836948cb00b00745592bd
Dreamsplutox <arnaudsimon091@gmail.com>
2020-05-03 19:08:58 +0200

    Exercice pipe en cours

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # 4IABD2_RUST_SHELL
 
-# 1 Révision de Rust et Processus
+# QUESTIONS DU TP
+
+## 1 Révision de Rust et Processus
 1) En Rust, les références servent à accéder à des ressources via leur adresse dans la mémoire, un peu comme les pointeurs en C
 2) Rust est compilé nativement, il n'y a pas de JVM
 3) Pour trouver la valeur maximale adressable, on doit calculer l'espace d'adressage avec la formule 2ⁿ - 1, ce qui nous donne ici pour 8 bits =>  255, en hexa FF. l'adresse maximale est donc 0x0000_0000_0000_00FF
-## 1.1 Pratique - micro-shell
-### 1 Créer un projet binaire avec cargo
+### 1.1 Pratique - micro-shell
+
+1) Créer un projet binaire avec cargo
 * Pour compiler et exécuter son programme, il faut utiliser la commande "cargo build" puis la commande "cargo run"  
 * Pour exécuter les tests, on doit utiliser cargo test
+* En mode debug, les binaires sont rangés dans le dossier target/debug, si on utilise l'option --release dans un cargo build, les binaires seront rangés dans le dossier target/release
+4) Rust nous force à récupérer le status pour que l'on puisse vérifier que le programme ne plante pas / que tout s'est bien passé
+5) Par défaut, le programme vit sa vie. Mais ici, "Status" force notre programme à attendre la fin de l'exécution de son enfant pour reprendre le travail
--- micro-shell/src/main.rs
+++ micro-shell/src/main.rs
@@ -1,11 +1,41 @@
 use std::io::{self, Write};
 use std::process::{Command};
+use std::vec::Vec;
 
+
+#[derive(Debug, Clone)]
+/// création de la structure avec les valeurs red, green et blue
+pub struct Cmd{
+    name : String,
+    args : Vec<String>
+}
+
+impl Cmd{
+    /// constructeur pour notre Cmd
+    pub fn new(name : String, args: Vec<String>) -> Self{
+        Cmd{
+            name: name,
+            args: args
+        }
+    }
+
+    /// getters
+    pub fn name(self) -> String{
+        self.name
+    }
+
+    pub fn args(self) -> Vec<String>{
+        self.args
+    }
+}
 
 fn main() -> std::io::Result<()>{
     let stdout = io::stdout();
 
     let stdin = io::stdin();
+
+    // Pour voir hello world avec strace
+    println!("Hello World");
 
     loop{
 
@@ -22,12 +52,26 @@ fn main() -> std::io::Result<()>{
         stdin.read_line(&mut user_input)?;
         let user_input = user_input.trim_end();
 
+        // Récupération des éléments entrés dans un vecteur
+        let mut i = 0;
+        let mut vec = Vec::new();
+
+        for token in user_input.split_whitespace(){
+            vec.push(token);
+            println!("vec {} {}",i,vec[i]);
+            i+=1;
+         }
+
         //-----------------------//
         // Executer une commande //
         //-----------------------//
-        let status = Command::new(user_input).status()?;
+        let output = Command::new(vec[0]).args(&vec[1..]).output()?;
         //afficher statut commande
-        println!("{}", status)
+        println!("status : {}", output.status);
+        let stdout = io::stdout().write_all(&output.stdout);
+        
+        //println!("stdout : {:?}", stdout);
+        println!("stderr : {:?}", output.stderr);
 
         //? sert à "propager l'erreur"
         // c'est mieux que crash avec un unwrap ou expect ;)
